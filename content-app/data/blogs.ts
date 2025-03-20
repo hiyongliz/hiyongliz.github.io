13b84f01ef58d4a20152f4c11ff6275f9bafce97
hiyongliz <644220198@qq.com>
2025-03-20 10:05:19 +0800

    删除不再使用的文件和配置，清理项目结构

--- content-app/data/blogs.ts
+++
@@ -1,6 +0,0 @@
-export const data = [
-  {
-    "title": "我的第一篇博客",
-    "date": "2024-10-18"
-  }
-];
--- content-app/data/weekly.ts
+++
@@ -1,6 +0,0 @@
-export const data = [
-  {
-    "title": "第一期周刊",
-    "date": "2024-10-18"
-  }
-];
